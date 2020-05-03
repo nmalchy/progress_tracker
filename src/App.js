cc70d71f7fdc189ac97bfb0ee89b516c2851cb1d
Noah Malchy <noahmalchy@gmail.com>
2020-05-03 09:35:20 -0700

    changed wrappers

--- src/App.js
+++ src/App.js
@@ -4,30 +4,30 @@ import './App.css';
 
 const Button = (props) => {
    return (
-   <React.Fragment>
+   <>
       <button type={props.type}>Submit</button>
-   </React.Fragment>
+   </>
    );
 }
 
 const Input = () => {
    return (
-      <React.Fragment>
+      <>
       <h1>Fitness Progress Tracker</h1>
       <form method='post' action='#'>
          <input type='text' placeHolder="Enter Weight" />
          <input type='text' placeHolder="Enter Body fat %" />
          <Button type="submit"/>
       </form>
-      </React.Fragment>
+      </>
    )
 }
 
 function App() {
    return (
-      <React.Fragment>
+      <>
       <Input />
-      </React.Fragment>
+      </>
       );
    }
 
